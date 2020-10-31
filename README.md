2c3faf97bbc7047d1a250764604792bf20c73840
Christophe Thibaut <cthibaut@octo.com>
2020-10-31 14:42:08 +0100

    load a font and display

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ C - TDD - allegro graphic library
 ## 01 - print Hello World on a graphic display
 
 ## 02 - make the display size a command line arg
+
+## 03 - install and use a font for display
--- src/lunar.c
+++ src/lunar.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include "allegro5/allegro5.h"
 #include "allegro5/allegro_font.h"
+#include "allegro5/allegro_ttf.h"
+#define FONT_NAME "Dosis Light 300.ttf"
 
 int main(int argc, char *argv[])
 {
@@ -19,8 +21,18 @@ int main(int argc, char *argv[])
     ALLEGRO_TIMER * timer = al_create_timer(1.0 / 30.0);
     ALLEGRO_EVENT_QUEUE * queue = al_create_event_queue();
     ALLEGRO_DISPLAY * display = al_create_display(display_size, display_size);
-    ALLEGRO_FONT * font = al_create_builtin_font();
 
+    al_init_font_addon();
+    al_init_ttf_addon();
+    ALLEGRO_PATH *path = al_get_standard_path(ALLEGRO_RESOURCES_PATH);
+    al_append_path_component(path, "../resources");
+    al_change_directory(al_path_cstr(path, '/'));
+    ALLEGRO_FONT * font = al_load_ttf_font(FONT_NAME, 32, 32);
+    if(!font) {
+        printf("unable to load ttf font:%s (path=%s)\n",FONT_NAME, al_path_cstr(path,'/'));
+        return 1;
+    }
+    al_destroy_path(path);
     al_register_event_source(queue, al_get_keyboard_event_source());
     al_register_event_source(queue, al_get_display_event_source(display));
     al_register_event_source(queue, al_get_timer_event_source(timer));
@@ -39,7 +51,7 @@ int main(int argc, char *argv[])
         if(redraw && al_event_queue_is_empty(queue))
         {
             al_clear_to_color(al_map_rgb(0, 0, 0));
-            al_draw_text(font, al_map_rgb(255, 255, 255), 0, 0, 0, "Hello world!");
+            al_draw_text(font, al_map_rgb(0, 255, 0), 0, 0, 0, "Hello world!");
             al_flip_display();
             redraw = false;
         }
